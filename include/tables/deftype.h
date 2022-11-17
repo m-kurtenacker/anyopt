8bcd9fb908c8841e9bd7348913a53726f15c39cb
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2022-11-17 13:11:00 +0100

    More defs added:
      * Insert
      * Bitcast
      * Global
      * Aggregates: Closure, StructAgg, Tuple, Vector
      * Filter
      * Known
      * Alloc
      * Select
      * Top, Bottom
      * SizeOf, AlignOf
      * DefiniteArray, IndefiniteArray

--- include/tables/deftype.h
+++ include/tables/deftype.h
@@ -5,15 +5,32 @@
 N("arithop", ArithOp) \
 N("continuation", Continuation) \
 N("const", Constant) \
+N("top", Top) \
+N("bottom", Bottom) \
 N("cmp", Cmp) \
 N("lea", LEA) \
 N("load", Load) \
 N("extract", Extract) \
+N("insert", Insert) \
 N("cast", Cast) \
+N("bitcast", Bitcast) \
 N("run", Run) \
 N("hlt", Hlt) \
 N("store", Store) \
 N("enter", Enter) \
 N("slot", Slot) \
+N("def_array", DefiniteArray) \
+N("indef_array", IndefiniteArray) \
+N("global", Global) \
+N("closure", Closure) \
+N("struct", Struct) \
+N("tuple", Tuple) \
+N("vector", Vector) \
+N("alloc", Alloc) \
+N("known", Known) \
+N("sizeof", Sizeof) \
+N("alignof", Alignof) \
+N("select", Select) \
+N("filter", Filter) \
 
 #endif
--- src/irbuilder.cpp
+++ src/irbuilder.cpp
@@ -10,8 +10,10 @@ IRBuilder::DefType IRBuilder::resolvedef (std::string def_type) {
     auto it = TypeMap.find(def_type);
     if (it != TypeMap.end())
         return it->second;
-    else
+    else {
+        std::cerr << "Unknown def type: " << def_type << std::endl;
         assert(false);
+    }
 }
 
 thorin::Array<const thorin::Def*> IRBuilder::get_arglist (json arg_list) {
@@ -36,16 +38,70 @@ const thorin::Def* IRBuilder::get_def (std::string type_name) {
 const thorin::Def * IRBuilder::build_Constant (json desc) {
     auto const_type = typetable_.get_type(desc["const_type"]);
     auto primtype = const_type->as<thorin::PrimType>();
+    assert(primtype->length() == 1);
+
     thorin::Box value;
     switch (primtype->tag()) {
 #define THORIN_I_TYPE(T, M) case thorin::PrimType_##T: { value = thorin::Box(desc["value"].get<thorin::M>()); break; }
 #define THORIN_BOOL_TYPE(T, M) case thorin::PrimType_##T: { value = thorin::Box(desc["value"].get<M>()); break; }
 #define THORIN_F_TYPE(T, M) case thorin::PrimType_##T: { value = thorin::Box(desc["value"].get<double>()); break; }
 #include <thorin/tables/primtypetable.h>
+    default:
+        assert(false && "not implemented");
     }
-    //thorin::Box value(desc["value"].get<thorin::s64>());
-    const thorin::Def* literal = world_.literal(primtype->primtype_tag(), value, {}, primtype->length());
+    const thorin::Def* literal = world_.literal(primtype->primtype_tag(), value, {});
     return literal;
+}
+
+const thorin::Def * IRBuilder::build_Top (json desc) {
+    auto const_type = typetable_.get_type(desc["const_type"]);
+    if (auto vector_type = const_type->isa<thorin::VectorType>())
+        assert(vector_type->length() == 1);
+
+    return world_.top(const_type);
+}
+
+const thorin::Def * IRBuilder::build_Bottom (json desc) {
+    auto const_type = typetable_.get_type(desc["const_type"]);
+    if (auto vector_type = const_type->isa<thorin::VectorType>())
+        assert(vector_type->length() == 1);
+
+    return world_.bottom(const_type);
+}
+
+const thorin::Def * IRBuilder::build_Alloc (json desc) {
+    auto args = get_arglist(desc["args"]);
+    auto target_type = typetable_.get_type(desc["target_type"]);
+
+    assert(args.size() == 2);
+
+    return world_.alloc(target_type, args[0], args[1]);
+}
+
+const thorin::Def * IRBuilder::build_Known (json desc) {
+    auto def = get_def(desc["def"]);
+
+    return world_.known(def);
+}
+
+const thorin::Def * IRBuilder::build_Sizeof (json desc) {
+    auto target_type = typetable_.get_type(desc["target_type"]);
+
+    return world_.size_of(target_type);
+}
+
+const thorin::Def * IRBuilder::build_Alignof (json desc) {
+    auto target_type = typetable_.get_type(desc["target_type"]);
+
+    return world_.align_of(target_type);
+}
+
+const thorin::Def * IRBuilder::build_Select (json desc) {
+    auto args = get_arglist(desc["args"]);
+
+    assert(args.size() == 3);
+
+    return world_.select(args[0], args[1], args[2]);
 }
 
 const thorin::Def * IRBuilder::build_Continuation (json desc) {
@@ -145,6 +201,14 @@ const thorin::Def * IRBuilder::build_Extract (json desc) {
     return world_.extract(args[0], args[1]);
 }
 
+const thorin::Def * IRBuilder::build_Insert (json desc) {
+    auto args = get_arglist(desc["args"]);
+
+    assert(args.size() == 3);
+
+    return world_.insert(args[0], args[1], args[2]);
+}
+
 const thorin::Def * IRBuilder::build_Cast (json desc) {
     auto target_type = typetable_.get_type(desc["target_type"]);
     auto source = get_def(desc["source"]);
@@ -205,6 +269,68 @@ const thorin::Def * IRBuilder::build_Slot (json desc) {
     auto frame = get_def(desc["frame"]);
 
     return world_.slot(target_type, frame);
+}
+
+const thorin::Def * IRBuilder::build_Bitcast (json desc) {
+    auto target_type = typetable_.get_type(desc["target_type"]);
+    auto source = get_def(desc["source"]);
+
+    return world_.bitcast(target_type, source);
+}
+
+const thorin::Def * IRBuilder::build_IndefiniteArray (json desc) {
+    auto elem_type = typetable_.get_type(desc["elem_type"]);
+    auto dim = get_def(desc["dim"]);
+
+    return world_.indefinite_array(elem_type, dim);
+}
+
+const thorin::Def * IRBuilder::build_DefiniteArray (json desc) {
+    auto elem_type = typetable_.get_type(desc["elem_type"]);
+    auto args = get_arglist(desc["args"]);
+
+    return world_.definite_array(elem_type, args);
+}
+
+const thorin::Def * IRBuilder::build_Global (json desc) {
+    bool is_mutable = desc["mutable"];
+    auto init = get_def(desc["init"]);
+
+    return world_.global(init, is_mutable);
+}
+
+const thorin::Def * IRBuilder::build_Closure (json desc) {
+    auto args = get_arglist(desc["args"]);
+    auto closure_type = typetable_.get_type(desc["closure_type"])->as<thorin::ClosureType>();
+
+    assert(args.size() == 2);
+
+    return world_.closure(closure_type, args[0], args[1]);
+}
+
+const thorin::Def * IRBuilder::build_Struct (json desc) {
+    auto args = get_arglist(desc["args"]);
+    auto struct_type = typetable_.get_type(desc["struct_type"])->as<thorin::StructType>();
+
+    return world_.struct_agg(struct_type, args);
+}
+
+const thorin::Def * IRBuilder::build_Tuple (json desc) {
+    auto args = get_arglist(desc["args"]);
+
+    return world_.tuple(args);
+}
+
+const thorin::Def * IRBuilder::build_Vector (json desc) {
+    auto args = get_arglist(desc["args"]);
+
+    return world_.vector(args);
+}
+
+const thorin::Def * IRBuilder::build_Filter (json desc) {
+    auto args = get_arglist(desc["args"]);
+
+    return world_.filter(args);
 }
 
 const thorin::Def * IRBuilder::reconstruct_def(json desc) {
